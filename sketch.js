6b67f3cea804cb02d2251be481fbc310084152ea
violeta-cf <violeta.cabrera.f@gmail.com>
2023-05-20 19:53:53 -0300

    Creamos el primer commit del proyecto

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+    createCanvas(600, 600);
+  
+    background(255);
+    strokeWeight(20);
+    
+        
+    fill(255, 255, 255);//BLANCO
+    rect(0,0, 200, 180);
+  
+     
+    fill(255, 255, 255);//BLANCO 
+    rect(0,180, 200,430);
+    
+    
+    fill(0, 0, 150);//AZUL
+    rect(0, 430, 200,200);
+    
+    fill(230, 0,0);//ROJO
+    rect(200, 0, 400,500);
+      
+    fill(255, 255, 255);//BLANCO
+    rect(200, 428, 600,300);
+    
+    
+    
+    fill(255, 255, 255);//BLANCO
+    rect(200, 428, 300,400);
+    
+      
+    fill(255, 225, 0);//ROJO
+    rect(500,500, 200,200);  
+    
+  }
